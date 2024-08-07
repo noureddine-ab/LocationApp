99e4cea7411f40560d3bc6a06864df29c6d347c6
noureddine <bounarda1234@gmail.com>
2024-08-07 13:50:14 +0100

    Location App for test

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.locationapp
 
+import android.content.Context
 import android.os.Bundle
+import android.Manifest
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.locationapp.ui.theme.LocationAppTheme
@@ -27,4 +35,43 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+
+@Composable
+fun LocationDisplay(LocationUtils:LocationUtils ,
+                    context : Context
+){
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract =ActivityResultContracts.RequestMultiplePermissions() ,
+        onResult = {permissions ->
+            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION]==true
+                && permissions[Manifest.permission.ACCESS_FINE_LOCATION]==true ){
+                // I have access to location
+            }else{
+                // Ask for permission
+            }
+
+        })
+
+
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Text(text = "Location Not Available")
+        Button(onClick = {
+            if (LocationUtils.hasLocationPermission(context)){
+                // Permission Already Granted update the location
+            }else{
+                // Request location permission
+            }
+        }) {
+            Text(text = "Get Location")
+        }
+    }
+
 }
